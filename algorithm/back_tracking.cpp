914227766d3b795f9b43568147fed36f4246887c
jun0.heo <gjwnsdud5076@gmail.com>
2026-09-27 04:45:03 +0900

    back tracking template

--- algorithm/back_tracking.cpp
+++ algorithm/back_tracking.cpp
@@ -17,43 +17,14 @@ void print(vector<vector<int>> vec){
     cout << "]" << endl;
 }
 
-vector<vector<int>> solutions;
-
-
-void back_tracking(int current, vector<int> track, vector<int> visited_, int sum_, int goal, int N){
-    if(find(visited_.begin(),visited_.end(),current) != visited_.end()) return;
-    track.push_back(current);
-    sum_ += current;
-    if(sum_ > goal) {
-        return;
-    } 
-    
-    if (sum_ == goal) {
-        solutions.push_back(track);
-        return;
-    }
-        
-    for(int i=current + 1; i<=N; i++){
-        if(find(visited_.begin(),visited_.end(),i) == visited_.end())
-            visited_.push_back(current);
-            back_tracking(i, track, sum_, goal, N);
-            visited_.pop_back();
-    }
-}
-
-void solution(int N) {
-    visited_.clear();
+vector<vector<int>> solution(int N) {
     for(int j=0; j<solutions.size(); j++){
         solutions[j].clear();
     }
     solutions.clear();
-    for(int i=1; i<=N; i++){
-        back_tracking(i, {}, {}, 0, 10, N);
-    }
 }
 
 int main() {
-    solution(7);
-    print(solutions);
+    print(solution(7));
     return 0;
 }
